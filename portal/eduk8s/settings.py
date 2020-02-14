1adbe8ddc9e3149f9f29bf819dcbe5a92db2d0f9
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-02-14 11:00:26 +1100

    Change name of directory for assembled static files.

--- portal/eduk8s/settings.py
+++ portal/eduk8s/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'eduk8s', 'static'),
