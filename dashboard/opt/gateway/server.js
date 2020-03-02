9087415eb7c517b81bd86b41660652b326dda742
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-02 14:03:20 +1100

    Force set path for cookie to be /.

--- dashboard/opt/gateway/server.js
+++ dashboard/opt/gateway/server.js
@@ -63,6 +63,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     sameSite: 'strict',
+    path: '/'
 }));
 
 // For standalone container deployment, provide the ability to enable
