946526b53fc5d6b0c2e6b265aa030dad6478a257
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-05 22:30:44 +1100

    Always add allowed redirect URI for applications.

--- portal/workshops/manager.py
+++ portal/workshops/manager.py
@@ -187,12 +187,15 @@ def process_workshop_environment(name, workshop, capacity):
 
         redirect_uris = [f"http://{session_hostname}/oauth_callback"]
 
-        ingress = status["workshop"]["spec"].get("session", {}).get("ingress", [])
+        for name in ["terminal", "console", "editor", "slides"]:
+            session_ingress_hostname = f"{session_name}-{name}.{domain}"
+            redirect_uris.append(f"http://{session_ingress_hostname}/oauth_callback")
 
-        for entry in ingress:
-            if entry.get("port", 10080) == 10080:
-                session_port_hostname = f"{session_name}-{entry['name']}.{domain}"
-                redirect_uris.append(f"http://{session_port_hostname}/oauth_callback")
+        ingresses = status["workshop"]["spec"].get("session", {}).get("ingresses", [])
+
+        for ingress in ingresses:
+            session_ingress_hostname = f"{session_name}-{ingress['name']}.{domain}"
+            redirect_uris.append(f"http://{session_ingress_hostname}/oauth_callback")
 
         eduk8s_user = User.objects.get(username="eduk8s")
 
