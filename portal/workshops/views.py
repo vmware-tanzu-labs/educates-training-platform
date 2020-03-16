e7eb4d6eb9325d4e51dac7fb8ce8cdffc3be3042
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-16 16:34:51 +1100

    Overriding variable in outer scope.

--- portal/workshops/views.py
+++ portal/workshops/views.py
@@ -75,9 +75,9 @@ def environment(request, environment):
                         state__in=["starting", "running"])
 
                 if active_sessions.count() < selected.capacity:
-                    session = initiate_workshop_session(selected)
+                    extra_session = initiate_workshop_session(selected)
                     transaction.on_commit(
-                            lambda: scheduler.create_workshop_session(name=session.name))
+                            lambda: scheduler.create_workshop_session(name=extra_session.name))
 
         else:
             # No session available. If there there is still capacity,
