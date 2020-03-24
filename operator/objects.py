3f98e6ae7c5d151293e58377f63e8c9b0a37b057
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-24 17:55:48 +1100

    Add fix broken Python client when creating CRDs.

--- operator/objects.py
+++ operator/objects.py
@@ -1,5 +1,30 @@
 import kubernetes.client
 
+import wrapt
+
+# XXX The Python swagger definitions are wrong result in creation of
+# CRDs failing. For now monkey patch the Python client to fix it.
+
+
+def fix_V1beta1CustomResourceDefinitionStatus___init__(wrapped, instance, args, kwargs):
+    def _resolve(accepted_names=None, conditions=[], stored_versions=[]):
+        return accepted_names, conditions, stored_versions
+
+    accepted_names, conditions, stored_versions = _resolve(*args, **kwargs)
+
+    return wrapped(
+        accepted_names=accepted_names,
+        conditions=conditions or [],
+        stored_versions=stored_versions or [],
+    )
+
+
+wrapt.wrap_function_wrapper(
+    "kubernetes.client.models.v1beta1_custom_resource_definition_status",
+    "V1beta1CustomResourceDefinitionStatus.__init__",
+    fix_V1beta1CustomResourceDefinitionStatus___init__,
+)
+
 # XXX The Python Kubernetes client doesn't currently support creating
 # custom resources using its create_from_dict() function, so we need to
 # do some hacks for now to get this to work for at least the eduk8s
