e2e6565254c1c8958433af5d2dc497d15cc08699
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-25 20:53:48 +1000

    Make whole objects read only.

--- portal/workshops/admin.py
+++ portal/workshops/admin.py
@@ -3,14 +3,15 @@ from django.contrib import admin
 from .models import Workshop, Session, Environment
 
 class WorkshopAdmin(admin.ModelAdmin):
-    readonly_fields = ('name', 'title', 'description', 'vendor', 'authors',
-            'difficulty', 'duration', 'tags', 'logo', 'url', 'image', 'content')
+    def has_change_permission(self, request, obj=None):
+        return False
 
 class EnvironmentAdmin(admin.ModelAdmin):
-    readonly_fields = ('name', 'workshop', 'capacity', 'reserved', 'tally', 'resource')
+    readonly_fields = ('name', 'workshop', 'initial', 'tally', 'resource')
 
 class SessionAdmin(admin.ModelAdmin):
-    readonly_fields = ('name', 'id', 'application', 'state', 'environment')
+    def has_change_permission(self, request, obj=None):
+        return False
 
 admin.site.register(Workshop, WorkshopAdmin)
 admin.site.register(Environment, EnvironmentAdmin)
