5cedd653105c83b0ccc50fdd9996e032d28ba34e
Kris De Volder <kdevolder@pivotal.io>
2020-06-26 15:09:01 -0700

    CORS

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,11 @@ export function activate(context: vscode.ExtensionContext) {
 
     const app: express.Application = express();
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.get("/hello", (req, res) => {
         res.send('Hello World!\n');
     });
