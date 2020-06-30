0a7ef30fba6539aa4643b5052c1e4bfcd09ea642
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-07-01 09:46:22 +1000

    Mark access code field as password so hidden when entered.

--- portal/workshops/forms.py
+++ portal/workshops/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class AccessTokenForm(forms.Form):
-    password = forms.CharField(label='Password', max_length=64, required=True)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(),
+            max_length=64, required=True)
     redirect_url = forms.CharField(widget=forms.HiddenInput(), required=True)
