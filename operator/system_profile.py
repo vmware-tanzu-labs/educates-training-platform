3b72e6c8acc1175b1b1e9d703a8ff3a39f3f2729
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-06-30 17:23:18 +1000

    Update to version 200630.071110.63e62a3 of portal image.

--- operator/system_profile.py
+++ operator/system_profile.py
@@ -14,7 +14,7 @@ __all__ = [
 
 default_image_repository = "quay.io/eduk8s"
 
-default_portal_image = "$(image_repository)/eduk8s-portal:200618.092922.d4ff8e9"
+default_portal_image = "$(image_repository)/eduk8s-portal:200630.071110.63e62a3"
 
 default_workshop_images = {
     "base-environment:*": "$(image_repository)/base-environment:200623.230753.88d94c3",
