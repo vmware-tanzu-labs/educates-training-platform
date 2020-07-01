4ddbcd686bf459e814af577fd6cbd5c52629954d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-07-01 13:48:12 +1000

    Add means to send text to all terminals for execution.

--- dashboard/opt/renderer/static/js/workshop-asciidoc.js
+++ dashboard/opt/renderer/static/js/workshop-asciidoc.js
@@ -103,6 +103,32 @@ $(document).ready(function() {
         }
     });
 
+    $.each([$('.execute-all .content')], function() {
+        if (window.location !== window.parent.location) {
+            $(this).find('.highlight').prepend('<span class="execute-glyph fas fa-running" aria-hidden="true"><sup><sup>*</sup></sup></span>');
+            this.parent().click(function(event) {
+                if (event.shiftKey) {
+                    $(this).find('.execute-glyph').removeClass('text-danger');
+                    $(this).find('.execute-glyph').addClass('text-success');
+                    handle_copy(event);
+                }
+                else {
+                    $(this).find('.execute-glyph').removeClass('text-success');
+                    $(this).find('.execute-glyph').addClass('text-danger');
+                    handle_execute(event, '*');
+                }
+                selectElementText(this);
+            });
+        } else {
+            $(this).find('.highlight').prepend('<span class="copy-glyph fas fa-copy" aria-hidden="true"></span>');
+            this.parent().click(function(event) {
+                $(this).find('.copy-glyph').addClass('text-success');
+                handle_copy(event);
+                selectElementText(this);
+            });
+        }
+    });
+
     $.each([$('.copypaste .content'), $('.copy .content')], function() {
         $(this).find('.highlight').prepend('<span class="copy-glyph fas fa-copy" aria-hidden="true"></span>');
         this.parent().click(function(event) {
