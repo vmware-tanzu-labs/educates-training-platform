2e61d81629e9c8436fd16d2d2c290f02276a785b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-07-28 14:47:55 +1000

    Only configure assets for slides if directory present.

--- dashboard/opt/gateway/src/backend/modules/slides.ts
+++ dashboard/opt/gateway/src/backend/modules/slides.ts
@@ -3,7 +3,7 @@ import * as express from "express"
 import { config } from "./config"
 
 export function setup_slides(app: express.Application) {
-    if (!config.enable_slides)
+    if (!config.enable_slides || !config.slides_dir)
         return
 
     app.use("/slides", express.static(config.slides_dir))
