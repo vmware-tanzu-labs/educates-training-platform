8005d41e4a33d0a1f3c44998de5afc0104fb2607
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-07-29 17:40:19 +1000

    Update to latest base environment images.

--- operator/system_profile.py
+++ operator/system_profile.py
@@ -14,19 +14,19 @@ __all__ = [
 
 default_image_repository = "quay.io/eduk8s"
 
-default_portal_image = "$(image_repository)/eduk8s-portal:200729.035511.95b0a4e"
+default_portal_image = "$(image_repository)/eduk8s-portal:200729.064453.9fa8ab6"
 
 default_workshop_images = {
-    "base-environment:*": "$(image_repository)/base-environment:200729.033538.484986c",
+    "base-environment:*": "$(image_repository)/base-environment:200729.065116.d64688f",
     "base-environment:develop": "$(image_repository)/base-environment:develop",
     "base-environment:master": "$(image_repository)/base-environment:master",
-    "jdk8-environment:*": "$(image_repository)/jdk8-environment:200729.040110.483ecc0",
+    "jdk8-environment:*": "$(image_repository)/jdk8-environment:200729.073011.4f0d6b3",
     "jdk8-environment:develop": "$(image_repository)/jdk8-environment:develop",
     "jdk8-environment:master": "$(image_repository)/jdk8-environment:master",
-    "jdk11-environment:*": "$(image_repository)/jdk11-environment:200729.040149.a94ec52",
+    "jdk11-environment:*": "$(image_repository)/jdk11-environment:200729.072954.5481ef8",
     "jdk11-environment:develop": "$(image_repository)/jdk11-environment:develop",
     "jdk11-environment:master": "$(image_repository)/jdk11-environment:master",
-    "conda-environment:*": "$(image_repository)/conda-environment:200729.035937.c70785f",
+    "conda-environment:*": "$(image_repository)/conda-environment:200729.073328.f5d8eff",
     "conda-environment:develop": "$(image_repository)/conda-environment:develop",
     "conda-environment:master": "$(image_repository)/conda-environment:master",
 }
