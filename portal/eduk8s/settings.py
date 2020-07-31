85873b820cefae5391674b8eb85fbd374e5b2bf5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-07-31 19:37:39 +1000

    Need to allow Google Analytics site for pulling images.

--- portal/eduk8s/settings.py
+++ portal/eduk8s/settings.py
@@ -179,7 +179,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CSP_DEFAULT_SRC = ("'none'", )
 CSP_STYLE_SRC = ("'self'", )
 CSP_SCRIPT_SRC = ("'self'", )
-CSP_IMG_SRC = ("'self'", "data:", "www.google-analytics.com")
+CSP_IMG_SRC = ("'self'", "data:", "www.google-analytics.com",
+        "www.googletagmanager.com")
 CSP_FONT_SRC = ("'self'", )
 CSP_FRAME_SRC = ("'self'", )
 CSP_INCLUDE_NONCE_IN = ("script-src", )
