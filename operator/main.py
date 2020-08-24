c83a07c2804909422358ab5a4581b84ac10bfcdc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-08-24 21:52:43 +1000

    Temporarily disable namespace reaper.

--- operator/main.py
+++ operator/main.py
@@ -1,2 +1,6 @@
 from handlers import *
-from daemons import *
+
+# XXX Disable the namespace reaper for now as it adds finalizer on namesapce
+# preventing the eduk8s namespace from being deleted when deleting operator.
+
+# from daemons import *
