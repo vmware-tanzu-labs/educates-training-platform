77942589272e397ae6f182c20d46899d5f3be34b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-09-09 11:59:49 +1000

    Allow presence of workshop to be configure through YAML.

--- dashboard/opt/gateway/src/backend/modules/dashboard.ts
+++ dashboard/opt/gateway/src/backend/modules/dashboard.ts
@@ -1,30 +1,9 @@
 import * as express from "express"
-import * as fs from "fs"
 
 import { config } from "./config"
 
-var enable_console = process.env.ENABLE_CONSOLE == "true"
-var enable_dashboard = process.env.ENABLE_DASHBOARD == "true"
-var enable_slides = process.env.ENABLE_SLIDES == "true"
-var enable_terminal = process.env.ENABLE_TERMINAL == "true"
-
-var google_tracking_id = process.env.GOOGLE_TRACKING_ID || ""
-
-var workshop_name = process.env.WORKSHOP_NAME || ""
-var session_namespace = process.env.SESSION_NAMESPACE || ""
-var workshop_namespace = process.env.WORKSHOP_NAMESPACE || ""
-var training_portal = process.env.TRAINING_PORTAL || ""
-var ingress_domain = process.env.INGRESS_DOMAIN || ""
-var ingress_protocol = process.env.INGRESS_PROTOCOL || "http"
-
-var portal_api_url = process.env.PORTAL_API_URL || ""
-
-var enable_portal = portal_api_url != ""
-
-var enable_countdown = process.env.ENABLE_COUNTDOWN == "true"
-
 export function setup_dashboard(app: express.Application) {
-    if (!enable_dashboard)
+    if (!config.enable_dashboard)
         return
 
     app.get("/dashboard/", (req, res) => {
