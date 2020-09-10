e50e81098d7249f51bc5cf2eb3b496e2ec18ebaa
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-09-10 11:41:18 +1000

    Switch to standard fedora base image.

--- src/eduk8s/settings.py
+++ src/eduk8s/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'workshops',
     'oauth2_provider',
     'corsheaders',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
@@ -81,9 +82,7 @@ WSGI_APPLICATION = 'eduk8s.wsgi.application'
 
 DATA_DIR = BASE_DIR
 
-if os.path.isdir('/var/run/eduk8s'):
-    DATA_DIR = '/var/run/eduk8s'
-elif os.path.isdir('/opt/app-root/data'):
+if os.path.isdir('/opt/app-root/data'):
     DATA_DIR = '/opt/app-root/data'
 
 DATABASES = {
