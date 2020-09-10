a0daeb7d02a2aae6292c1cb36459489ca7b195df
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-09-10 16:21:03 +1000

    Rename Django project from eduk8s to config.

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for eduk8s project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
 # This monkey patch allows setting of SameSite to 'None' while wait for
@@ -34,7 +25,7 @@ def _wrapper_set_cookie(wrapped, instance, args, kwargs):
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eduk8s.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 
