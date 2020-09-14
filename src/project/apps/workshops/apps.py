9fb8995159bf59cf0f77b9f27e83788b86f8e5bc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-09-14 16:47:43 +1000

    Clean up application and forms modules.

--- src/project/apps/workshops/apps.py
+++ src/project/apps/workshops/apps.py
@@ -1,5 +1,13 @@
+"""Django application objects.
+
+"""
+
 from django.apps import AppConfig
 
 
 class WorkshopsConfig(AppConfig):
+    """Config for workshops application.
+
+    """
+
     name = 'workshops'
