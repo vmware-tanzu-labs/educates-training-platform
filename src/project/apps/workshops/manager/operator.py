e443a9e1e6d5d5b9c07bfc134b1e26dc396bd6a8
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-10-12 20:16:31 +1100

    Add documentation.

--- src/project/apps/workshops/manager/operator.py
+++ src/project/apps/workshops/manager/operator.py
@@ -19,12 +19,22 @@ import kopf
 _event_loop = None  # pylint: disable=invalid-name
 
 
-def event_loop():
-    return _event_loop
-
-
 class Task:
-    def __init__(self, wrapped, name, delay, repeat, args, kwargs):
+    """Encapsulation of an instance of a background task. It binds the
+    function implementing the task with the arguments to use when calling it,
+    along with details of when to call it and whether it should run as a
+    periodic task.
+
+    """
+
+    def __init__(
+        self, wrapped, name, delay, repeat, args, kwargs
+    ):  # pylint: disable=too-many-arguments
+        """Capture details of the function implementing the task and the
+        parameters for invoking it.
+
+        """
+
         self.wrapped = wrapped
         self.name = name
         self.delay = delay
@@ -33,9 +43,17 @@ class Task:
         self.kwargs = kwargs
 
     def execute(self):
+        """Execute the function for the task synchronously."""
+
         return self.wrapped(*self.args, **self.kwargs)
 
     def schedule(self, *, delay=None):
+        """Schedule a the task to be run using asyncio library. The delay
+        before running the task which was originally specified can be
+        overridden at this point if necessary.
+
+        """
+
         delay = delay or self.delay
 
         async def sleep():
@@ -44,13 +62,26 @@ class Task:
 
         @sync_to_async
         def execute():
+            """Executes the function, capturing details of any exception and
+            logging it since nothing will even wait on the results.
+
+            """
+
             try:
-                logging.info("Executing task %s %s %s.", self.name, self.args, self.kwargs)
+                logging.info(
+                    "Executing task %s %s %s.", self.name, self.args, self.kwargs
+                )
                 return self.wrapped(*self.args, **self.kwargs)
+
             except Exception:  # pylint: disable=broad-except
                 logging.exception("Exception raised by task %s.", self.name)
 
         async def task():
+            """The asynchronous task wrapper for the function. Handles once
+            off execution as well as looping forever with delay.
+
+            """
+
             await sleep()
 
             if self.repeat:
@@ -61,6 +92,11 @@ class Task:
             else:
                 return await execute()
 
+        # This schedules the task to be run by the asyncio loop. This is
+        # done using threadsafe registration mechanism as can be called by
+        # request handler thread of mod_wsgi and not necessarily a function
+        # executing under asyncio.
+
         return asyncio.run_coroutine_threadsafe(task(), _event_loop)
 
 
@@ -70,10 +106,19 @@ def background_task(wrapped=None, *, name=None, delay=0.0, repeat=False):
 
     """
 
+    # Parameters for the task are optional so if any were supplied return
+    # a partially bound version of the decorator function for subsequent
+    # applyication to the function implementing the task.
+
     if wrapped is None:
         return functools.partial(background_task, delay=delay, repeat=repeat)
 
     name = name or wrapped.__qualname__
+
+    # Return a function which encapsulates the details of the task and which
+    # in turn returns a task wrapper object. Code using this still needs to
+    # called either execute() or schedule() on the task object which is
+    # returned.
 
     def wrapper(*args, **kwargs):
         return Task(wrapped, name, delay, repeat, args, kwargs)
@@ -82,7 +127,7 @@ def background_task(wrapped=None, *, name=None, delay=0.0, repeat=False):
 
 
 def initialize_kopf():
-    """Run kopf in a separate thread and register shutdown handler with
+    """Run kopf in a separate thread and register a shutdown handler with
     mod_wsgi to ensure we clean things up properly on process shutdown.
 
     """
@@ -99,14 +144,9 @@ def initialize_kopf():
         global _event_loop  # pylint: disable=invalid-name,global-statement
 
         _event_loop = asyncio.new_event_loop()
-
         asyncio.set_event_loop(_event_loop)
 
         with contextlib.closing(_event_loop):
-            # Turn on verbose logs from kopf framework.
-
-            # kopf.configure(log_format="%(levelname)s:%(name)s - %(message)s")
-
             # Run event loop until flagged to shutdown.
 
             _event_loop.run_until_complete(
@@ -116,6 +156,9 @@ def initialize_kopf():
         logging.info("Exiting kopf framework main loop.")
 
     thread = Thread(target=worker)
+
+    # Register the shutdown handler with mod_wsgi so everything cleaned
+    # up properly when the process is being stopped.
 
     def shutdown_handler(*_, **__):
         stop_flag.set()
