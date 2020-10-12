fcf1fe7b275eb90c00e197c37dedf7722c3eeca5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-10-12 21:09:37 +1100

    Update documentation.

--- src/project/apps/workshops/manager/cleanup.py
+++ src/project/apps/workshops/manager/cleanup.py
@@ -25,7 +25,7 @@ from .operator import background_task
 
 api = pykube.HTTPClient(pykube.KubeConfig.from_env())
 
-WorkshopSession = pykube.object_factory(
+K8SWorkshopSession = pykube.object_factory(
     api, "training.eduk8s.io/v1alpha1", "WorkshopSession"
 )
 
@@ -33,15 +33,24 @@ WorkshopSession = pykube.object_factory(
 @background_task(delay=15.0, repeat=True)
 @scheduler_lock
 def purge_expired_workshop_sessions():
+    """Look for workshop sessions which have expired and delete them."""
+
     now = timezone.now()
 
-    ingress_protocol = settings.INGRESS_PROTOCOL
-    ingress_domain = settings.INGRESS_DOMAIN
+    # Loop over all records of workshop sessions in the database and check
+    # whether any should be deleted and/or marked as stopped.
 
     for session in Session.objects.all():
         if not session.is_stopped():
+            # If the workshop session hasn't stopped yet, check to see whether
+            # there is a deployed workshop session. If there isn't, it means
+            # it was deleted manually. In this case trigger a task to clean up
+            # the workshop session. In this case there will be no deployment
+            # to delete, but still have to mark the workshop session as
+            # deleted in the database.
+
             try:
-                WorkshopSession.objects(api).get(name=session.name)
+                K8SWorkshopSession.objects(api).get(name=session.name)
 
             except pykube.exceptions.ObjectDoesNotExist:
                 logging.info("Session %s missing. Cleanup session.", session.name)
@@ -53,7 +62,13 @@ def purge_expired_workshop_sessions():
             except pykube.exceptions.PyKubeError:
                 pass
 
-        if session.is_allocated() or session.is_stopping():
+        elif session.is_allocated() or session.is_stopping():
+            # If the workshop session is in use, including where it has been
+            # explicitly marked for expiration, if expiration time has been
+            # reached we need to delete it. If expiration time hasn't been
+            # reached and there is an inactivity timeout, check that it hasn't
+            # been orhpaned.
+
             if session.expires and session.expires <= now:
                 logging.info("Session %s expired. Deleting session.", session.name)
 
@@ -61,10 +76,20 @@ def purge_expired_workshop_sessions():
 
             elif session.environment.inactivity:
                 try:
-                    url = f"{ingress_protocol}://{session.name}.{ingress_domain}/session/activity"
+                    # Query the idle time from the workshop session instance.
+
+                    host = f"{session.name}.{settings.INGRESS_DOMAIN}"
+                    url = f"{settings.INGRESS_PROTOCOL}://{host}/session/activity"
+
                     response = requests.get(url)
+
                     if response.status_code == 200:
+                        # If got a response and we have exceeded the
+                        # inactivity timeout then trigger deletion of the
+                        # workshop session.
+
                         idle_time = timedelta(seconds=response.json()["idle-time"])
+
                         if idle_time >= session.environment.inactivity:
                             logging.info(
                                 "Session %s orphaned. Deleting session.", session.name
@@ -72,19 +97,32 @@ def purge_expired_workshop_sessions():
 
                             delete_workshop_session(session).schedule()
 
+                    else:
+                        # XXX If we don't get a valid response then not
+                        # currently doing anything. Need a better method to
+                        # determine if was running but has since failed in
+                        # some way and become uncontactable. In that case
+                        # right now will only be deleted when workshop timeout
+                        # expires if there is one.
+
+                        pass
+
                 except requests.exceptions.ConnectionError:
-                    # XXX
-                    # This can just be because it is slow to start up. Need
-                    # a better method to determine if was running but has since
-                    # failed in some way and become uncontactable. In that case
-                    # right now will only be deleted when workshop timeout
-                    # expires if there is one.
+                    # XXX This can just be because it is slow to start up.
+                    # Need a better method to determine if was running but has
+                    # since failed in some way and become uncontactable. In
+                    # that case right now will only be deleted when workshop
+                    # timeout expires if there is one.
 
                     logging.warning(
                         "Cannot connect to workshop session %s.", session.name
                     )
 
                 except Exception:  # pylint: disable=broad-except
+                    # Not aware of circumstances where would get an unexpected
+                    # exception, but need to log and ignore it as we don't
+                    # want to stop looping over all sessions.
+
                     logging.error(
                         "Failed to query idle time for workshop session %s.",
                         session.name,
@@ -96,8 +134,14 @@ def purge_expired_workshop_sessions():
 @background_task
 @scheduler_lock
 def delete_workshop_session(session):
+    """Deletes a workshop session."""
+
+    # First attempt to delete the deployment of the workshop session. It
+    # doesn't matter if it doesn't exist. That situation can arise where
+    # the workshop session was deleted manually for some reason.
+
     try:
-        resource = WorkshopSession.objects(api).get(name=session.name)
+        resource = K8SWorkshopSession.objects(api).get(name=session.name)
         resource.delete()
 
     except pykube.exceptions.ObjectDoesNotExist:
@@ -107,6 +151,10 @@ def delete_workshop_session(session):
         logging.error("Failed to delete workshop session %s.", session.name)
 
         traceback.print_exc()
+
+    # Update the workshop session as stopped in the database, then see
+    # whether a new workshop session needs to be created in its place as
+    # a reserved session. 
 
     with transaction.atomic():
         session.mark_as_stopped()
@@ -137,7 +185,7 @@ def cleanup_old_sessions_and_users():
             session.delete()
 
         # Delete any anonymous users older than 36 hours old, which
-        # now have an workshop sessions associated with them.
+        # now don't have any workshop sessions associated with them.
 
         User = get_user_model()  # pylint: disable=invalid-name
 
