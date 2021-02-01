cf83aa9156f7eb957937d986b5db254163930355
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-02-01 11:51:18 +1100

    Update to latest training portal image.

--- operator/system_profile.py
+++ operator/system_profile.py
@@ -14,7 +14,7 @@ __all__ = [
 
 default_image_repository = "quay.io/eduk8s"
 
-default_portal_image = "$(image_repository)/eduk8s-portal:210128.065403.b1dcd60"
+default_portal_image = "$(image_repository)/eduk8s-portal:210201.004426.9f72dba"
 
 default_workshop_images = {
     "base-environment:*": "$(image_repository)/base-environment:201203.020609.1ab533d",
