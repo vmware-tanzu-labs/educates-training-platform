e116733abdb5cedc7451375fc62e59e55fae7b2e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-03-18 14:37:10 +1100

    Update to workshop and portal images providing analytics via webhook URL.

--- operator/system_profile.py
+++ operator/system_profile.py
@@ -14,19 +14,19 @@ __all__ = [
 
 default_image_repository = "quay.io/eduk8s"
 
-default_portal_image = "$(image_repository)/eduk8s-portal:210316.104146.d3a127b"
+default_portal_image = "$(image_repository)/eduk8s-portal:210318.022435.7422e74"
 
 default_workshop_images = {
-    "base-environment:*": "$(image_repository)/base-environment:210311.051557.4f20b2f",
+    "base-environment:*": "$(image_repository)/base-environment:210318.030713.6329673",
     "base-environment:develop": "$(image_repository)/base-environment:develop",
     "base-environment:master": "$(image_repository)/base-environment:master",
-    "jdk8-environment:*": "$(image_repository)/jdk8-environment:210311.052803.e672f94",
+    "jdk8-environment:*": "$(image_repository)/jdk8-environment:210318.032956.b7ea0f4",
     "jdk8-environment:develop": "$(image_repository)/jdk8-environment:develop",
     "jdk8-environment:master": "$(image_repository)/jdk8-environment:master",
-    "jdk11-environment:*": "$(image_repository)/jdk11-environment:210311.052723.45f9930",
+    "jdk11-environment:*": "$(image_repository)/jdk11-environment:210318.032932.da2d87b",
     "jdk11-environment:develop": "$(image_repository)/jdk11-environment:develop",
     "jdk11-environment:master": "$(image_repository)/jdk11-environment:master",
-    "conda-environment:*": "$(image_repository)/conda-environment:210311.052651.fa7d2dc",
+    "conda-environment:*": "$(image_repository)/conda-environment:210318.032837.a8e93f0",
     "conda-environment:develop": "$(image_repository)/conda-environment:develop",
     "conda-environment:master": "$(image_repository)/conda-environment:master",
 }
