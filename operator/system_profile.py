efb461809408568c7ae41a0ebbb263f881a6c630
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-03-23 19:58:30 +1100

    Update workshop base images with editor clickable actions fix.

--- operator/system_profile.py
+++ operator/system_profile.py
@@ -17,16 +17,16 @@ default_image_repository = "quay.io/eduk8s"
 default_portal_image = "$(image_repository)/eduk8s-portal:210322.001416.32babe5"
 
 default_workshop_images = {
-    "base-environment:*": "$(image_repository)/base-environment:210318.104148.bbecec4",
+    "base-environment:*": "$(image_repository)/base-environment:210323.083558.62baa67",
     "base-environment:develop": "$(image_repository)/base-environment:develop",
     "base-environment:master": "$(image_repository)/base-environment:master",
-    "jdk8-environment:*": "$(image_repository)/jdk8-environment:210318.110827.c22628f",
+    "jdk8-environment:*": "$(image_repository)/jdk8-environment:210323.084709.aeb5c9e",
     "jdk8-environment:develop": "$(image_repository)/jdk8-environment:develop",
     "jdk8-environment:master": "$(image_repository)/jdk8-environment:master",
-    "jdk11-environment:*": "$(image_repository)/jdk11-environment:210318.110808.7e6a0cc",
+    "jdk11-environment:*": "$(image_repository)/jdk11-environment:210323.084650.be7aa62",
     "jdk11-environment:develop": "$(image_repository)/jdk11-environment:develop",
     "jdk11-environment:master": "$(image_repository)/jdk11-environment:master",
-    "conda-environment:*": "$(image_repository)/conda-environment:210318.110609.65e2d9a",
+    "conda-environment:*": "$(image_repository)/conda-environment:210323.084629.dbd0a3d",
     "conda-environment:develop": "$(image_repository)/conda-environment:develop",
     "conda-environment:master": "$(image_repository)/conda-environment:master",
 }
