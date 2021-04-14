73274ddb475504e055bace2cf0e479393991e7ab
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-04-14 12:56:27 +1000

    Use MyST parser for Markdown.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
