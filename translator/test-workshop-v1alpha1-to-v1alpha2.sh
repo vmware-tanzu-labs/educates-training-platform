059e8f3082c63cc63824f494d41cb2c6365f3965
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-04-22 19:25:30 +1000

    Removed aborted translator code.

--- translator/test-workshop-v1alpha1-to-v1alpha2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-HERE=`dirname $0`
-
-curl -s --header "Content-Type: application/json" \
-  --data "`cat $HERE/test-workshop-v1alpha1-to-v1alpha2.json`" \
-  http://localhost:8080/api/translate | jq
