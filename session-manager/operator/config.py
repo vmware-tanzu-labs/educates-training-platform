ea0ec2ddef619079cd4214ad86adbcdac2a26016
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2022-04-06 10:56:24 +1000

    Pass through package data values for naming to operator container.

--- session-manager/operator/config.py
+++ session-manager/operator/config.py
@@ -6,7 +6,7 @@ if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/namespace"):
     with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace") as fp:
         OPERATOR_NAMESPACE = fp.read().strip()
 
-OPERATOR_API_GROUP = "eduk8s.io"
+OPERATOR_API_GROUP = os.environ.get("OPERATOR_API_GROUP", "eduk8s.io")
 
-RESOURCE_STATUS_KEY = "eduk8s"
-RESOURCE_NAME_PREFIX = "eduk8s"
+RESOURCE_STATUS_KEY = os.environ.get("RESOURCE_STATUS_KEY", "eduk8s")
+RESOURCE_NAME_PREFIX = os.environ.get("RESOURCE_NAME_PREFIX", "eduk8s")
