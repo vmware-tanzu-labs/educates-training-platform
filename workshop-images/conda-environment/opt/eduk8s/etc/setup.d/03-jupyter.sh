3b80b1d2d0427924eed73ffd969b0ae34a3c47b9
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2022-07-29 12:52:03 +1000

    Update default password handling for JupyterLab.

--- workshop-images/conda-environment/opt/eduk8s/etc/setup.d/03-jupyter.sh
+++ workshop-images/conda-environment/opt/eduk8s/etc/setup.d/03-jupyter.sh
@@ -5,12 +5,12 @@ if [ ! -f $HOME/.condarc ]; then
     conda config --prepend pkgs_dirs $HOME/.conda/pkgs
 fi
 
-if [ ! -f $HOME/.jupyter/jupyter_notebook_config.json ]; then
+if [ ! -f $HOME/.jupyter/jupyter_server_config.json ]; then
     mkdir -p $HOME/.jupyter
-    cat > $HOME/.jupyter/jupyter_notebook_config.json << EOF
+    cat > $HOME/.jupyter/jupyter_server_config.json << 'EOF'
 {
-  "NotebookApp": {
-    "password": "sha1:1c5cec6a0e25:fc2cc26cf297f0760a49957bb570be0f06c525f0"
+  "ServerApp": {
+    "password": "argon2:$argon2id$v=19$m=10240,t=10,p=8$JZCmwQwwLK/qMvPpnWO+BQ$gfTKcrEH1YXkzRo3GWL9CLCOaWIPvKBmgOLJ1RDhvc0"
   }
 }
 EOF
