62eea83e5146b893b2716301ee3e446cc1644a4d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2022-10-21 14:27:56 +1100

    Add command to open workshop hosted in docker.

--- client-programs/educates/cmd/docker.go
+++ client-programs/educates/cmd/docker.go
@@ -17,6 +17,7 @@ func NewDockerCmd() *cobra.Command {
 		NewDockerWorkshopDeployCmd(),
 		NewDockerWorkshopDeleteCmd(),
 		NewDockerWorkshopsListCmd(),
+		NewDockerWorkshopOpenCmd(),
 	)
 
 	return dockerCmd
