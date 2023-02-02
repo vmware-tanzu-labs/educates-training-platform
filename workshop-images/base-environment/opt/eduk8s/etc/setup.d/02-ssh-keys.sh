2a0ab824e6c26366c6e90a886ab0787c8ae5bd4d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2023-02-02 20:36:47 +1100

    Create the SSH id_rsa.pub file as well as private key file.

--- workshop-images/base-environment/opt/eduk8s/etc/setup.d/02-ssh-keys.sh
+++ workshop-images/base-environment/opt/eduk8s/etc/setup.d/02-ssh-keys.sh
@@ -10,7 +10,8 @@ if [ -f $HOME/.ssh/id_rsa -o ! -f /opt/ssh-keys/private.pem ]; then
     exit 0
 fi
 
-# Copy the SSH private key file into place.
+# Copy the SSH private/public key file into place. We need to convert the public
+# key to rsa format in the process.
 
 mkdir -p $HOME/.ssh
 
@@ -19,3 +20,7 @@ chmod 0700 $HOME/.ssh
 cp /opt/ssh-keys/private.pem $HOME/.ssh/id_rsa
 
 chmod 0600 $HOME/.ssh/id_rsa
+
+ssh-keygen -f /opt/ssh-keys/public.pem -i -m PKCS8 > $HOME/.ssh/id_rsa.pub
+
+chmod 0600 $HOME/.ssh/id_rsa.pub
