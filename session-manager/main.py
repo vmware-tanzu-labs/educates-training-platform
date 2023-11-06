285f63692a3f3c90c1ddceb8fca256ffce23039f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2023-11-06 13:47:58 +1100

    Set connection times for kopf to attempt to workaround REST API connections blocking.

--- session-manager/main.py
+++ session-manager/main.py
@@ -31,6 +31,8 @@ _stop_flag = Event()
 @kopf.on.startup()
 def configure(settings: kopf.OperatorSettings, **_):
     settings.posting.level = logging.DEBUG
+    settings.watching.connect_timeout = 1 * 60
+    settings.watching.server_timeout = 10 * 60
 
 
 @kopf.on.login()
