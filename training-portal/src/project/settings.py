d1ede5e3dd9157b145a140e2e8a7c65ac61dc64f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2024-05-23 10:17:32 +1000

    Remove duplicate config setting.

--- training-portal/src/project/settings.py
+++ training-portal/src/project/settings.py
@@ -235,7 +235,6 @@ CSP_CONNECT_SRC = (
 
 CSP_DEFAULT_SRC = ("'none'",)
 CSP_STYLE_SRC = ("'self'",)
-CSP_SCRIPT_SRC = ("'self'",)
 CSP_SCRIPT_SRC = ("'self'", "www.clarity.ms", "cdn.amplitude.com")
 CSP_IMG_SRC = (
     "'self'",
