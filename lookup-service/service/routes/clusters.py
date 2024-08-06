82471cea8144f3e078f2292d27c2a71fed4a0810
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2024-08-06 16:25:07 +1000

    Add means to get portals from clusters.

--- lookup-service/service/routes/clusters.py
+++ lookup-service/service/routes/clusters.py
@@ -29,7 +29,7 @@ async def api_get_v1_clusters(request: web.Request) -> web.Response:
 async def api_get_v1_clusters_details(request: web.Request) -> web.Response:
     """Returns details for the specified cluster."""
 
-    cluster_name = request.match_info["name"]
+    cluster_name = request.match_info["cluster"]
 
     service_state = request.app["service_state"]
     cluster_database = service_state.cluster_database
@@ -52,7 +52,7 @@ async def api_get_v1_clusters_details(request: web.Request) -> web.Response:
 async def api_get_v1_clusters_kubeconfig(request: web.Request) -> web.Response:
     """Returns a kubeconfig file for the specified cluster."""
 
-    cluster_name = request.match_info["name"]
+    cluster_name = request.match_info["cluster"]
 
     service_state = request.app["service_state"]
     cluster_database = service_state.cluster_database
@@ -67,10 +67,45 @@ async def api_get_v1_clusters_kubeconfig(request: web.Request) -> web.Response:
     return web.Response(text=kubeconfig)
 
 
+@login_required
+@roles_accepted("admin", "cluster-reader")
+async def api_get_v1_clusters_portals(request: web.Request) -> web.Response:
+    """Returns a list of portals for the specified cluster."""
+
+    cluster_name = request.match_info["cluster"]
+
+    service_state = request.app["service_state"]
+    cluster_database = service_state.cluster_database
+
+    cluster = cluster_database.get_cluster(cluster_name)
+
+    if not cluster:
+        return web.Response(text="Cluster not available", status=403)
+
+    data = {
+        "portals": [
+            {
+                "name": portal.name,
+                "uid": portal.uid,
+                "generation": portal.generation,
+                "labels": portal.labels,
+                "url": portal.url,
+                "capacity": portal.capacity,
+                "allocated": portal.allocated,
+                "phase": portal.phase,
+            }
+            for portal in cluster.get_portals()
+        ]
+    }
+
+    return web.json_response(data)
+
+
 # Set up the routes for the cluster management API.
 
 routes = [
     web.get("/api/v1/clusters", api_get_v1_clusters),
-    web.get("/api/v1/clusters/{name}", api_get_v1_clusters_details),
-    web.get("/api/v1/clusters/{name}/kubeconfig", api_get_v1_clusters_kubeconfig),
+    web.get("/api/v1/clusters/{cluster}", api_get_v1_clusters_details),
+    web.get("/api/v1/clusters/{cluster}/kubeconfig", api_get_v1_clusters_kubeconfig),
+    web.get("/api/v1/clusters/{cluster}/portals", api_get_v1_clusters_portals),
 ]
